374827b7a4868bc0637ccac791f32f5e80cf22c9
gabriel_cpereira <gabriel.cpereira@totvs.com.br>
2023-01-13 16:13:11 -0300

    feat: add upload function

--- src/screens/Upload.tsx
+++ src/screens/Upload.tsx
@@ -1,14 +1,32 @@
 import { StyleSheet, Button } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
 
 export default function Upload({ navigation }: RootStackScreenProps<'Upload'>) {
+  const readFile = async (uri: string) => {
+    const children = await FileSystem.readAsStringAsync(uri);
+    const lines = String(children).replace(/[\n|\n\r]+/g, '#').split('#');
+    lines.forEach((line: string) => {
+      const data = line.replace(/\s+/g, '#').split('#');
+      console.log(data);
+    });
+  };
+
+  const pickDocument = async () => {
+    const document = await DocumentPicker.getDocumentAsync({ type: 'application/octet-stream' });
+
+    if (document.type === 'success') readFile(document.uri);
+    else alert('Falha ao selecionar o arquivo. Tente novamente!');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Upload</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Button title='Home' onPress={() => navigation.navigate('Home')} />
+      <Button title='Home' onPress={pickDocument} />
     </View>
   );
 }
